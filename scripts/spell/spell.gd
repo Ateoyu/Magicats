22fc241747eb89bf8aaadf7500f948686c368156
Misakuja <aniatu13@gmail.com>
2026-03-23 14:04:24 +0100

    Add a basic spell

---
+++ scripts/spell/spell.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export_category("Base Stats")
+@export var level: int = 100
+@export var hp: int = 10
+@export var speed: int = 50
+@export var damage: int = 50
+@export var attack_size: int = 50
+
+#TODO: implement
