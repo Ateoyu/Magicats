56fc1f984efdd7a482d6e2cf911194e7cc991b34
Ateoyu <ateosu@proton.me>
2026-03-25 17:19:03 +0100

    Add `LightningStrike` spell with unique projectile logic and animations, update `.gitignore` for IDE files

---
+++ scripts/spell/lightning_strike.gd
@@ -0,0 +1,39 @@
+class_name LightningStrike
+extends Spell
+
+var projectile_count: int
+var aoe_radius: float
+var lightning_projectile: PackedScene = preload("res://scenes/spell/lightningStrike.tscn")
+
+func _init(p_spell_manager: Node2D, p_player: CharacterBody2D) -> void:
+	super(1, 10, 2.0, p_spell_manager, p_player)
+	self.projectile_count = 3
+	self.aoe_radius = 50.0
+
+func cast() -> void:
+	var targets_hit: Array[Enemy] = []
+
+	for i in projectile_count:
+		var target_enemy: Enemy = get_unique_target(targets_hit)
+		if target_enemy != null:
+			targets_hit.append(target_enemy)
+			
+			var projectile: LightningStrikeProjectile = lightning_projectile.instantiate()
+			projectile.global_position = target_enemy.global_position
+			projectile.initialize(target_enemy.global_position, damage, aoe_radius)
+	
+			spell_manager.add_child(projectile)
+
+func get_unique_target(already_targeted: Array) -> Enemy :
+	if player.enemy_close.size() == 0:
+		return null
+
+	var available_enemies: Array[Enemy] = []
+	for enemy in player.enemy_close:
+		if not already_targeted.has(enemy):
+			available_enemies.append(enemy)
+
+	if available_enemies.size() > 0:
+		return available_enemies.pick_random()
+	else:
+		return null
