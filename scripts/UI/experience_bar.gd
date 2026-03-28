ab65d7080a76b32ec9a8fa5b50a2053f66387496
Ateoyu <ateosu@proton.me>
2026-03-28 16:01:10 +0100

    Add ExperienceManager

--- scripts/UI/experience_bar.gd
+++ scripts/UI/experience_bar.gd
@@ -4,14 +4,12 @@ extends ProgressBar
 @export var level_label: Label
 
 func _ready() -> void:
-	if player:
-		player.update_experience_bar.connect(_update_experience_bar)
-		_update_experience_bar()
+	ExperienceManager.update_experience_bar.connect(_update_experience_bar)
+	_update_experience_bar()
 
 func _update_experience_bar() -> void:
-	if player:
-		value = player.experience
-		max_value = player.calculate_experience_cap()
-		
-		if level_label:
-			level_label.text = str("Level: ", player.experience_level)
+	value = ExperienceManager.experience
+	max_value = ExperienceManager.calculate_experience_cap()
+	
+	if level_label:
+		level_label.text = str("Level: ", ExperienceManager.experience_level)
